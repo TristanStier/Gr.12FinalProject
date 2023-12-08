cc2685be19a7166cade32bdc1ccd41e4f9db0224
TristanStier <stier.tristan@gmail.com>
2023-12-08 09:39:39 -0500

    Fixed Bug
    Fixed the system_fingerprint error and turned off memory to save tokens.

--- Assets/Scripts/Converstaion/NpcOpenAI.cs
+++ Assets/Scripts/Converstaion/NpcOpenAI.cs
@@ -62,10 +62,18 @@ namespace OpenAI
 
         private async void summarizeConversation(string name, List<ChatMessage> messages)
         {
+            // // INDIVIDUAL INTERACTIONS MEMORY CODE
+            // var summarizePrompt = new ChatMessage()
+            // {
+            //     Role = "user",
+            //     Content = "You can break character here and return to your default settings. I want you to summarize your past conversation given the context of your past interactions. Here is a summary/memory of your past interactions outside of your current conversation: " + mInteractionsSummary + ". I now want you to summarize your current conversation given the context of your previous interactions into a summary that will act as a memory for your conversation. It should be written in first person in the format of: \" I met a man named ..., we talking a little bit about this and he left.\". Make sure you only summarize the key talking points and important ideas into a single short and concise summary. Don't include anything else in the summary. It is important to put [Interaction (respective summary number): ] before each summary so I can add it into a list of memories and keep it in chronological order for future reference. You also should express your point of view on the topic in the summary. The summary is only for the past conversation and it must include important specifics rather than general idea so you can reference them in the future. If you ever make up an answer after being asked a specific question about yourself, make sure to include it in the summary and indicate that you said it about yourself."
+            // };
+
+            // BIG SUMMARY MEMORY CODE
             var summarizePrompt = new ChatMessage()
             {
                 Role = "user",
-                Content = "You can break character here and return to your default settings. I want you to summarize your past conversation given the context of your past interactions. Here is a summary/memory of your past interactions outside of your current conversation: " + mInteractionsSummary + ". I now want you to summarize your current conversation given the context of your previous interactions into a summary that will act as a memory for your conversation. It should be written in first person in the format of: \" I met a man named ..., we talking a little bit about this and he left.\". Make sure you only summarize the key talking points and important ideas into a single short and concise summary. Don't include anything else in the summary. It is important to put [Interaction (respective summary number): ] before each summary so I can add it into a list of memories and keep it in chronological order for future reference. You also should express your point of view on the topic in the summary. The summary is only for the past conversation and it must include important specifics rather than general idea so you can reference them in the future. If you ever make up an answer after being asked a specific question about yourself, make sure to include it in the summary and indicate that you said it about yourself."
+                Content = "You can break character here and return to your default settings. I want you to summarize your past interactions and your current conversation into one big summary to serve as a memeroy for future encounters. Here is a summary/memory of your past interactions outside of your current conversation: " + mInteractionsSummary + ". I now want you to summarize your current conversation given the previous messages and then mix it in with your past interactions outside of the last converstaion to create 1 large recolection of memories. It should be written in first person in the format of: \" I met a man named ..., we talking a little bit about this and he left.\". Make sure you only summarize the key talking points and important ideas into a single short and concise summary. Don't include anything else in the summary other than your paste interactions with the converstaion you juts had."
             };
 
             messages.Add(summarizePrompt);
@@ -80,9 +88,9 @@ namespace OpenAI
             if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
             {
                 var message = completionResponse.Choices[0].Message;
-                mInteractionsSummary += "\n" + message.Content.ToString();
+                mInteractionsSummary = message.Content.ToString(); // Big memories
+                // mInteractionsSummary += "\n" + message.Content.ToString(); // Individual memories            
                 print(mInteractionsSummary);
-                messages.Clear();
             }
         }
 
@@ -119,8 +127,8 @@ namespace OpenAI
         public void endConversation()
         {
             mInteracting = false;
-
-            summarizeConversation(mPlayerInteraction.getName(), mMessages);
+            // summarizeConversation(mPlayerInteraction.getName(), mMessages);
+            mMessages.Clear();
         }
 
         public string getName()
