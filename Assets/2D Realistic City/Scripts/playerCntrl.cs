f89dd8a429f846c5641b1811cce107ca92edc0c7
TristanStier <stier.tristan@gmail.com>
2023-12-20 22:05:58 -0500

    Imported City
    Imported assets and sprites to make the city.

---
+++ Assets/2D Realistic City/Scripts/playerCntrl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCntrl : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+	//void Start () {
+	//rb = GetComponent <Rigidbody2D> ();
+	//}
+	
+	
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		
+	}
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
